2687e0f8df53f1360dacc6c05f949813a3236fce
Matt Thornton <mattthornton@workfront.com>
2016-05-11 11:23:17 -0600

    removing react specific and unused code

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import thunk from 'redux-thunk'
-import createStore from './createStore'
-import applyMiddleware from './applyMiddleware';
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-export default createStoreWithMiddleware();
